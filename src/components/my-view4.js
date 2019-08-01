d1e175f7b8a3271660da0ac3c727b2bfd74a0b0a
sdyalor <sdyalor@outlook.com>
2019-08-01 17:59:29 +0000

    add modelos and render dropdowns, clean code

--- src/components/my-view4.js
+++ src/components/my-view4.js
@@ -13,6 +13,26 @@ class MyView4 extends PageViewElement {
     super();
     this.value = 1 ;
     this.vehiculos = [];
+    this.marcas = [];
+    this.modelos = [];
+    this.fetchModelos = fetch('http://azaryah.sdyalor.me/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+     body: JSON.stringify({query: `
+     query {	
+      modelo {
+        codModelo
+        descripcion
+      }
+    
+    }
+     `})
+    })
+      .then(r => r.json())
+      .then(data => this.modelos = (data['data']['modelo']));
     this.fetchVehiculos = fetch('http://azaryah.sdyalor.me/api/graphql', {
       method: 'POST',
       headers: {
@@ -23,6 +43,7 @@ class MyView4 extends PageViewElement {
      query {	
       vehiculo {
         codVehiculo
+        placa
       }
     
     }
@@ -30,337 +51,24 @@ class MyView4 extends PageViewElement {
     })
       .then(r => r.json())
       .then(data => this.vehiculos = (data['data']['vehiculo']));
-    this.snneumaticosdet =
-      [
-        {
-          "codNeumatico": "0856",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017108",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017109",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017112",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017115",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017116",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017117",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017118",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017119",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017125",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017127",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017128",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017130",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017131",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017134",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017135",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017136",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017138",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017139",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "101017140",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "101017143",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10111712",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10114036",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10114039",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10114063",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "10115011",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115014",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115017",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115018",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115021",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "10115043",
-          "codEvento": "10",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "1"
-        },
-        {
-          "codNeumatico": "10115171",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115175",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115179",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10115180",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "1011523",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "1011606",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10116100",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10116101",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10116102",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        },
-        {
-          "codNeumatico": "10116103",
-          "codEvento": "19",
-          "codObra": "01",
-          "usuario": "roberto",
-          "fecha": "2019-05-31",
-          "idcarga": "2"
-        }]
-
+    this.fetchMarcas = fetch('http://azaryah.sdyalor.me/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+     body: JSON.stringify({query: `
+     query {	
+      marca {
+        codMarca
+        descripcion
+      }
+    
+    }
+     `})
+    })
+      .then(r => r.json())
+      .then(data => this.marcas = (data['data']['marca']));
   }
   static get styles() {
     return [
@@ -370,6 +78,8 @@ class MyView4 extends PageViewElement {
   static get properties() {
     return {
       vehiculos: { type: Array},
+      marcas: {type: Array},
+      modelos: {type: Array}
     };
   }
 
@@ -377,45 +87,72 @@ class MyView4 extends PageViewElement {
     return html`
     <style is="custom-style" include="paper-item-shared-styles"></style>
     <section>
-    <!-- comment -->
-     <paper-dropdown-menu label="Neumaticos" vertical-offset="60">
-       <paper-listbox slot="dropdown-content" selected=3 >
-        ${this.snneumaticosdet.map(item => html`<paper-item @click=${e => console.log(e.target.innerText)}>${item['codNeumatico']}</paper-item>`)}
-       </paper-listbox>
-     </paper-dropdown-menu>
-    <!-- comment -->
-     <paper-dropdown-menu label="Obra" vertical-offset="60">
-       <paper-listbox slot="dropdown-content" selected=3 >
-        ${this.snneumaticosdet.map(item => html`<paper-item @click=${e => console.log(e.target.innerText)}>${item['codObra']}</paper-item>`)}
-       </paper-listbox>
-     </paper-dropdown-menu>
-    <!-- comment -->
-     <paper-dropdown-menu label="fecha" vertical-offset="60">
-       <paper-listbox slot="dropdown-content" selected=3 >
-        ${this.snneumaticosdet.map(item => html`<paper-item @click=${e => console.log(e.target.innerText)}>${item['fecha']}</paper-item>`)}
-       </paper-listbox>
-     </paper-dropdown-menu>
-    <!-- comment -->
-     <paper-dropdown-menu label="Evento" vertical-offset="60">
-       <paper-listbox slot="dropdown-content" selected=3 >
-        ${this.snneumaticosdet.map(item => html`<paper-item @click=${e => console.log(e.target.innerText)}>${item['codEvento']}</paper-item>`)}
-       </paper-listbox>
-     </paper-dropdown-menu>
-
-    <!-- comment -->
-
+    <!-- codVehiculo -->
      <paper-dropdown-menu label="Array Vehiculos" vertical-offset="60" async>
        <paper-listbox slot="dropdown-content" selected=3 >
-    ${this.boolVehiculos?
-      html`<paper-item>no content</paper-item>`:
+    ${
       html`${
-             this.vehiculos.map( x => html`<paper-item>${x.codVehiculo}</paper-item>`)
+             this.vehiculos.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.codVehiculo}</paper-item>`)
             }`
     }
        </paper-listbox>
      </paper-dropdown-menu>
-
-
+    <!-- end codVehiculo -->
+    <!-- placa -->
+     <paper-dropdown-menu label="Placa de Array Vehiculos vehiculos" vertical-offset="60" async>
+       <paper-listbox slot="dropdown-content" selected=3 >
+    ${
+      html`${
+            this.vehiculos.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.placa}</paper-item>`)
+            }`
+    }
+       </paper-listbox>
+     </paper-dropdown-menu>
+    <!-- end placa -->
+    <!-- codMarca -->
+     <paper-dropdown-menu label="codMarca de Array Marcas" vertical-offset="60" async>
+       <paper-listbox slot="dropdown-content" selected=3 >
+    ${
+      html`${
+            this.marcas.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.codMarca}</paper-item>`)
+            }`
+    }
+       </paper-listbox>
+     </paper-dropdown-menu>
+    <!-- End codMarca -->
+    <!-- Marca Detalle -->
+     <paper-dropdown-menu label="descripcion de Array Marcas" vertical-offset="60" async>
+       <paper-listbox slot="dropdown-content" selected=3 >
+    ${
+      html`${
+            this.marcas.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.descripcion}</paper-item>`)
+            }`
+    }
+       </paper-listbox>
+     </paper-dropdown-menu>
+    <!-- End Marca Detalle -->
+    <!-- Modelos Detalle -->
+     <paper-dropdown-menu label="codModelo de Array modelos" vertical-offset="60" async>
+       <paper-listbox slot="dropdown-content" selected=3 >
+    ${
+      html`${
+            this.modelos.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.codModelo}</paper-item>`)
+            }`
+    }
+       </paper-listbox>
+     </paper-dropdown-menu>
+    <!--End  Modelos Detalle -->
+    <!-- Modelos Detalle -->
+     <paper-dropdown-menu label="descripcion de Array modelos" vertical-offset="60" async>
+       <paper-listbox slot="dropdown-content" selected=3 >
+    ${
+      html`${
+            this.modelos.map( x => html`<paper-item @click=${e => console.log(e.target.innerText)}>${x.descripcion}</paper-item>`)
+            }`
+    }
+       </paper-listbox>
+     </paper-dropdown-menu>
+    <!--End  Modelos Detalle -->
     </section>
      <style>
          button, p {
