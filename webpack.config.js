1a6ce1b634a1e41898e723607f3d9c99e8b7d7bf
sdyalor <sdyalor@outlook.com>
2019-10-05 21:54:54 +0000

    typescript branch

--- webpack.config.js
+++ webpack.config.js
@@ -20,25 +20,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|js)x?$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', {
-                targets: {ie: '11'},
-                useBuiltIns: 'entry'
-              }]
+              ['@babel/preset-env', {targets: {ie: '11'}}]
             ],
             plugins: ['@babel/plugin-syntax-dynamic-import']
           }
         }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
   },
   plugins: [
     new CopyWebpackPlugin([
       'images/**',
+      'node_modules/@webcomponents/webcomponentsjs/**',
       'manifest.json'
     ]),
     new HtmlWebpackPlugin({
@@ -47,11 +52,16 @@ module.exports = {
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       include: ['index.html', 'manifest.json', /\.js$/],
+      exclude: [/\/@webcomponents\/webcomponentsjs\//],
       navigateFallback: 'index.html',
       swDest: 'service-worker.js',
       clientsClaim: true,
       skipWaiting: true,
       runtimeCaching: [
+        {
+          urlPattern: /\/@webcomponents\/webcomponentsjs\//,
+          handler: 'staleWhileRevalidate'
+        },
         {
           urlPattern: /^https:\/\/fonts.gstatic.com\//,
           handler: 'staleWhileRevalidate'
